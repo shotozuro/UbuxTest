c8ff05d22d68665c00910feb054b153305632696
Shalahuddin Fajri <shotozuro@gmail.com>
2019-06-02 02:48:25 +0700

    add propt-types to Separator

--- components/Separator.js
+++ components/Separator.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import { View } from 'react-native'
+import PropTypes from 'prop-types'
+import Colors from '../constants/Colors'
 
 export default class Separator extends React.Component {
   render () {
-    return (<View style={{ height: 1, backgroundColor: '#f0f0f0', flex: 1, alignSelf: 'stretch' }} />)
+    return (<View style={{
+      flex: 1,
+      alignSelf: 'stretch',
+      backgroundColor: this.props.height,
+      height: this.props.height
+    }} />)
   }
+}
+
+Separator.propTypes = {
+  height: PropTypes.number,
+  color: PropTypes.string
+}
+
+Separator.defaultProps = {
+  height: 1,
+  color: Colors.gray
 }
